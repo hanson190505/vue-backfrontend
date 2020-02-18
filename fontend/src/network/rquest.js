d1c7239893cb189efb63f0cf034aabebb59e0576
hanson190505 <413506012@qq.com>
2020-02-19 04:58:40 +0800

    online test-2

--- fontend/src/network/rquest.js
+++ fontend/src/network/rquest.js
@@ -6,9 +6,10 @@ let postData = null
 export function request(config) {
   // 创建一个axios实例
   const instance = axios.create({
-    baseURL: 'http://0.0.0.0:8000/',
+    // baseURL: 'http://0.0.0.0:8000/',
     // baseURL: 'http://192.168.3.45:8000/',
     // baseURL: 'http://3.135.197.102:8000/',
+    baseURL: 'http: //cms.chinagoodgifts.com',
     timeout: 5000,
   })
   //axios拦截器(请求局部拦截)
@@ -52,7 +53,8 @@ export function request(config) {
 
 export function getRangeDateRequest(config) {
   const instance = axios.create({
-    baseURL: 'http://0.0.0.0:8000/',
+    baseURL: 'http: //cms.chinagoodgifts.com',
+    // baseURL: 'http://0.0.0.0:8000/',
     // baseURL: 'http://192.168.3.45:8000/orders/',
     // baseURL: 'http://3.135.197.102:8000/orders/',
     timeout: 5000,
